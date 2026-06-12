4a64a264bba9536e4d226521c0b68901d084d3a8
dipakbhatt-09 <bhattdipak90@gmail.com>
2026-06-12 19:34:20 +0545

    final production settings update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 
 # SECURITY
 SECRET_KEY = os.environ.get(
     "SECRET_KEY",
-    "django-insecure-7ku=4zv6(p26c5jf&z%)r^lpcmvc%g7u2d=q6nz+!enaeplj+6"
+    "django-insecure-7ku=4zv6(p26c5jf%z%)r^lpcmvc%g7u2d=q6nz+!enaeplj+6"
 )
 
 DEBUG = False
@@ -21,9 +21,7 @@ ALLOWED_HOSTS = [
 ]
 
 
-
 # APPLICATIONS
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -72,8 +70,11 @@ SIMPLE_JWT = {
 
 # MIDDLEWARE
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
+    "corsheaders.middleware.CorsMiddleware",
+
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -106,14 +107,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-
-# DATABASE 
+# DATABASE (POSTGRES + SQLITE fallback)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
+
 
 # PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
@@ -131,17 +131,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-
-# STATIC , MEDIA
+# STATIC / MEDIA
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-# CORS (FRONTEND - REACT)
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+# CORS (FRONTEND)
+CORS_ALLOW_ALL_ORIGINS = True
